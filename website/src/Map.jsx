49e0708f8a7af40d7eae40fd197e24c9e77df0a7
George <zhang21@uw.edu>
2021-05-31 13:03:55 -0700

    fully working map

--- website/src/Map.jsx
+++ website/src/Map.jsx
@@ -10,10 +10,40 @@ class Map extends Component {
         console.log(Object.keys(this.props.location.state.jsonData).length);
     }
 
+
+ districtStyle = {
+        fillColor: '#FF4500',
+        opacity: 0.7,
+        color: "black",
+    };
+
     onEachDistrict = (district, layer) =>{
-        const districtName = district.properties["2016"]
-        //console.log(district);
+        let districtName = district.properties["2016"]
+        let districtClone = districtName;
+        const jsonData = this.props.location.state.jsonData;
+        //console.log(typeof jsonData);
+        //console.log(jsonData[1].PRIORITY);
+        //console.log(jsonData.length);
+        let priority = 0;
+        for (let i = 0; i < 128; i++) {
+            if(jsonData[i].DISTRICT === districtName) {
+                districtName += " <br> Priority: " + jsonData[i].PRIORITY + " <br> Vaccines to give: "
+                 + Math.floor(jsonData[i].NUM_VACCINE);
+                priority = jsonData[i].PRIORITY;
+            }
+        }
+
         layer.bindPopup(districtName);
+        layer.options.fillOpacity = 0.7;
+        layer.options.fillColor = priority === 5 ? '#CA0B00' :
+                                                 priority === 4  ? '#FF4500	' :
+                                                 priority === 3  ? '#ffa500' :
+                                                 priority === 2  ? '#FF5733' :
+                                                 priority === 1  ? '#FFC300' :
+                                                 '#000000';
+        if(districtClone === "NOYA" || districtClone === "NEBBI" || districtClone === "KITGUM") {
+                layer.options.fillColor = '#900C3F';
+                }
     }
 
       render() {
@@ -30,10 +60,12 @@ class Map extends Component {
     //           </Popup>
     //         </Marker>
     //       </MapContainer>
-
+    <div>
+    <h1 style={{textAlign: "center"}}> Uganda Vaccine Allocation </h1>
     <MapContainer center={[1.347532, 32.510201]} zoom={7} scrollWheelZoom={true}>
-    <GeoJSON data={districts.features} onEachFeature={this.onEachDistrict} />
+    <GeoJSON style={this.districtStyle} data={districts.features} onEachFeature={this.onEachDistrict} />
     </MapContainer>
+    </div>
         );
       }
     }
